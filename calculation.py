05d704b377314e6062d43b1f2f95371cbadd873f
kcy919 <kcy919@naver.com>
2020-08-26 11:20:14 +0900

    commit

---
+++ calculation.py
@@ -0,0 +1,16 @@
+
+
+a = 3
+b = 4
+
+def plus(a,b):
+    c= a+b
+    return c
+
+def subtract(a,b):
+    c= a-b
+    return c
+
+if __name__ == "__main__":
+    print("a + b=", subtract(a,b))
+    print("a - b=", plus(a,b))
---
+++ math_1.py
@@ -0,0 +1,5 @@
+def_main():
+    print("안녕하세요, main() 입니다.")
+
+if__name__ == "__maim__":
+    main()
